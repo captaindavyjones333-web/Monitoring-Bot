693f1687d9455d3f54c6e5f27ed3601e8e580837
Hayk-Darbinyan <haykdarbinyan04@gmail.com>
2026-06-29 15:24:48 +0400

    refactor: add cash files paths in 3d and mobile

--- scrapers/d3planet.js
+++ scrapers/d3planet.js
@@ -247,6 +247,9 @@ export async function scrape3DPlanet() {
 
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH ||
+      "/opt/render/.cache/puppeteer/chrome/linux-150.0.7871.24/chrome-linux64/chrome",
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
